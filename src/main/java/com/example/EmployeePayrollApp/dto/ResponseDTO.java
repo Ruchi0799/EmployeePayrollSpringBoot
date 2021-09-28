fdc33f1add44ecb9c4cb4f8cddbc1d9df184d613
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-28 14:22:42 +0530

    [ruchi]: implemented employeepayroll lambok logger in Spring boot

--- src/main/java/com/example/EmployeePayrollApp/dto/ResponseDTO.java
+++ src/main/java/com/example/EmployeePayrollApp/dto/ResponseDTO.java
@@ -12,19 +12,19 @@ public ResponseDTO(String message, Object data) {
 	this.setData(data);
 }
 
-public Object getData() {
-	return data;
-}
-
-public void setData(Object data) {
-	this.data = data;
-}
-
-public String getMessage() {
-	return message;
-}
-
-public void setMessage(String message) {
-	this.message = message;
-}
+//public Object getData() {
+//	return data;
+//}
+//
+//public void setData(Object data) {
+//	this.data = data;
+//}
+//
+//public String getMessage() {
+//	return message;
+//}
+//
+//public void setMessage(String message) {
+//	this.message = message;
+//}
 }
