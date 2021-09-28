667bb0d6a032438bd22c19ff30fe22b2e546e264
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-28 12:50:22 +0530

    [ruchi]: implemented employeepayroll lambok in Spring boot

--- src/main/java/com/example/EmployeePayrollApp/dto/ResponseDTO.java
+++ src/main/java/com/example/EmployeePayrollApp/dto/ResponseDTO.java
@@ -1,25 +1,30 @@
 package com.example.EmployeePayrollApp.dto;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public @Data class ResponseDTO {
+	
 private String message;
 private Object data;
+
 public ResponseDTO(String message, Object data) {
-	super();
-	this.message = message;
-	this.data = data;
+	this.setMessage(message);
+	this.setData(data);
 }
-public String getMessage() {
-	return message;
-}
-public void setMessage(String message) {
-	this.message = message;
-}
+
 public Object getData() {
 	return data;
 }
+
 public void setData(Object data) {
 	this.data = data;
 }
 
+public String getMessage() {
+	return message;
+}
 
+public void setMessage(String message) {
+	this.message = message;
+}
 }
